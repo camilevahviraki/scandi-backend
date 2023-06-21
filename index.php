65807f41eb6e5f8452e816db73c821b54b4653fe
camilevahviraki <camilevahviraki@gmail.com>
2023-06-21 23:44:00 +0200

    Update index file

--- index.php
+++ index.php
@@ -8,4 +8,14 @@ $router = new HandleRouter($conn);
 $route = $_SERVER['REQUEST_URI'];
 $router->getRoute($route);
 
+// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//     if ($_SERVER['REQUEST_URI'] === '/deleteProducts') {
+//         $ids = $_POST['data'];
+//         $dataR = (object) ['message' => 'Invalid product IDs', 'sentData' => $ids, 'products' => []];
+//         $dataR = json_encode($dataR);
+//         header('Content-Type: application/json');
+//         echo json_encode($dataR);
+//     }
+// }
+
 ?>
